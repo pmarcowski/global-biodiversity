491587b792c810395b774c4b62ea55f8598cb577
Przemysław Marcowski <p.marcowski@gmail.com>
2024-05-28 21:47:10 -0700

    Add data preparation

---
+++ prepare_data.R
@@ -0,0 +1,45 @@
+# Title: Processing of biodiversity data
+# Author: Przemyslaw Marcowski, PhD
+# Email: p.marcowski@gmail.com
+# Date: 2024-02-14
+# Copyright (c) 2024 Przemyslaw Marcowski
+
+# This code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+# This script performs data preparation of the biodiversity dataset.
+
+# Load packages
+library(vroom)
+library(data.table)
+
+# Read occurrence data
+occurence_data <- vroom("./data/occurence.csv", col_select = c(
+  "id", "continent", "country",
+  "scientificName", "vernacularName", 
+  "eventDate", "longitudeDecimal", "latitudeDecimal"
+))
+
+# Convert to data.table and prepare
+occurence_data <- as.data.table(occurence_data)
+occurence_data <- occurence_data[country == "Poland"]
+
+# Read media data
+media_data <- vroom("./data/multimedia.csv", col_select = c("CoreId", "accessURI", "creator"))
+
+# Convert to data.table and prepare
+media_data <- as.data.table(media_data)
+setnames(media_data, "CoreId", "id")
+
+# Perform a left join to add accessURI and creator to occurrence data
+occurence_data <- merge(occurence_data, media_data, by = "id", all.x = TRUE)
+
+# Create a lookup table from occurence_data for scientificName to accessURI and creator
+lookup <- occurence_data[!is.na(accessURI), .(accessURI = first(accessURI), creator = first(creator)), by = scientificName]
+
+# Fill NAs in accessURI and creator in occurrence data using the lookup table
+occurence_data[is.na(accessURI), `:=`(accessURI = lookup[.SD, on = .(scientificName), accessURI], 
+                                      creator = lookup[.SD, on = .(scientificName), creator])]
+
+# Save prepared occurrence data to app directory
+saveRDS(occurence_data, "./app/data/occurence_prepared.Rds")
